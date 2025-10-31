4be663fb0799c86c7140f219215e9da753e64a2d
Резников Константин Павлович <KPReznikov@sberbank.ru>
2025-10-31 14:02:02 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Монтируем статические файлы (CSS, JS, изображения)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Настраиваем шаблонизатор
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+@app.get("/items/{item_id}", response_class=HTMLResponse)
+async def read_item(request: Request, item_id: str):
+    return templates.TemplateResponse(
+        "item.html",
+        {
+            "request": request,
+            "item_id": item_id,
+            "example_data": ["Статистика", "Графики", "Отчёты"]
+        }
+    )
